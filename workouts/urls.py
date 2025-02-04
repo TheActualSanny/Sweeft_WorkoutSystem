028daedc4e9ee59ae0701781146625ceeb2cdfac
TheActualSanny <dugna1234@gmail.com>
2025-02-04 17:58:35 +0400

    refactor: add new urls

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema = get_schema_view(
+    openapi.Info(title = 'Workouts Manager', default_version = '1.0.0', 
+                 description =  'The documentation of the personalized workouts API'),
+    public = True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('account_auth.urls')),
+    path('api/v1/', schema.with_ui('swagger', cache_timeout = 0), name = 'documentation'),
+    path('', include('account_auth.urls'), name = 'AUTH'),
     path('home/', include('workouts_main.urls')),
     path('plans/', include('workouts_planner.urls')),
     path('tracking/', include('tracking.urls'))
